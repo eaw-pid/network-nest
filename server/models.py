b423270bc8a399b2c4d1b1d4f2d8e7a98b32f830
Erin Wise <erinwise88@gmail.com>
2024-02-20 17:15:32 -0500

    create user db and start working on NavBar

--- server/models.py
+++ server/models.py
@@ -1,6 +1,21 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    # _password_hash = db.Column(db.String, nullable=False)
+
+##add validations, including password_hash validations and bcrypt
+
--- server/seed.py
+++ server/seed.py
@@ -7,11 +7,35 @@ from random import randint, choice as rc
 from faker import Faker
 
 # Local imports
-from app import app
-from models import db
+from config import app, db
+from models import User
 
-if __name__ == '__main__':
-    fake = Faker()
+if __name__ == "__main__":
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        ##Clear Existing Data
+        db.session.query(User).delete()
+
+    #Create users
+        user1 = User(username = "Tester", first_name = "Bill", last_name = "Murray", email = "tester123@gmail.com")
+
+        db.session.add(user1)
+        db.session.commit()
+
+# fake = Faker
+
+# def make_users():
+#     User.query.delete()
+
+#     users = []
+#     for i in range(10):
+#         u = User(username = fake.name(), email = fake.email())
+#         users.append(u)
+    
+#     db.session.add_all(users)
+#     db.session.commit()
+
+# if __name__ == '__main__':
+#     fake = Faker()
+#     with app.app_context():
+#         make_users()
+#         # Seed code goes here!
